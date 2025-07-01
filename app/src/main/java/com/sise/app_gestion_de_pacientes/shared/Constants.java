7181be935fb7747145cf5dbd3609d042341c36cb
AnthonyPc26 <anthonypachasc@gmail.com>
2025-07-01 01:23:14 -0500

    Guardado

--- app/src/main/java/com/sise/app_gestion_de_pacientes/shared/Constants.java
+++ app/src/main/java/com/sise/app_gestion_de_pacientes/shared/Constants.java
@@ -1,10 +1,6 @@
 package com.sise.app_gestion_de_pacientes.shared;
 
 public class Constants {
-<<<<<<< Updated upstream
-    public static String BASE_URL_API="http://192.168.80.31:3000/api/v1";
-=======
     public static String BASE_URL_API = "http://192.168.80.31:3000/api/v1";
->>>>>>> Stashed changes
-    //Poner la IP de cada uno al momento de probar el app
+
 }
