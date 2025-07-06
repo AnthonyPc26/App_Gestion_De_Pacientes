624c45250c8bae84ed8721bdd0b79d7228d407d0
AnthonyPc26 <anthonypachasc@gmail.com>
2025-07-05 20:14:41 -0500

    Diseno

--- app/src/main/java/com/sise/app_gestion_de_pacientes/shared/Constants.java
+++ app/src/main/java/com/sise/app_gestion_de_pacientes/shared/Constants.java
@@ -1,7 +1,7 @@
 package com.sise.app_gestion_de_pacientes.shared;
 
 public class Constants {
-    public static String BASE_URL_API = "http://192.168.0.130:3000/api/v1";
+    public static String BASE_URL_API = "http://192.168.80.31:3000/api/v1";
     public static String SHARED_PREFERENCES_USUARIO_LOGUEADO = "USUARIO_LOGUEADO";
 
 
